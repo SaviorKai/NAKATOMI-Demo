060c6c1970efee2dacdea767813e968a482a9b41
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2020-05-21 17:15:10 +1000

    Rebuilt Pinching + Implemented Testing Pinch by Mouse Wheel + Implemented Planet Size Scale Testing + Created New class for Spring arm.
    To Do:
    - build C++ class on new spring arm
    - implement new spring arm in BP
    - Have arms scale correctly while pinching

---
+++ NakatomiDemo/Source/NakatomiDemo/ND_PlanetArmComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright by Ivan Engelbrecht 2020. All Rights reserved.
+
+
+#include "ND_PlanetArmComponent.h"
+
---
+++ NakatomiDemo/Source/NakatomiDemo/ND_PlanetArmComponent.h
@@ -0,0 +1,17 @@
+// Copyright by Ivan Engelbrecht 2020. All Rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "ND_PlanetArmComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAKATOMIDEMO_API UND_PlanetArmComponent : public USpringArmComponent
+{
+	GENERATED_BODY()
+	
+};
