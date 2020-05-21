570f216bcaa1235a86ceb4c336cff7bbefa21d25
SaviorKai <55675002+SaviorKai@users.noreply.github.com>
2020-05-21 18:29:46 +1000

    Implemented the New C++ Planet Spring Arm Component and added it to all the planets

--- NakatomiDemo/Source/NakatomiDemo/ND_PlanetArmComponent.cpp
+++ NakatomiDemo/Source/NakatomiDemo/ND_PlanetArmComponent.cpp
@@ -1,5 +1,23 @@
 // Copyright by Ivan Engelbrecht 2020. All Rights reserved.
 
-
 #include "ND_PlanetArmComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
+// Sets default values
+UND_PlanetArmComponent::UND_PlanetArmComponent()
+{
+	bDoCollisionTest = false;
+	SetRelativeLocation(FVector(0.0f, 0.0f, 40.0f));
+}
+
+void UND_PlanetArmComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	DefaultArmLength = TargetArmLength;
+}
+
+float UND_PlanetArmComponent::GetDefaultArmLength()
+{
+	return DefaultArmLength;
+}
--- NakatomiDemo/Source/NakatomiDemo/ND_PlanetArmComponent.h
+++ NakatomiDemo/Source/NakatomiDemo/ND_PlanetArmComponent.h
@@ -9,9 +9,22 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( ClassGroup = (PlanetParts), meta = (BlueprintSpawnableComponent) )
 class NAKATOMIDEMO_API UND_PlanetArmComponent : public USpringArmComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	UND_PlanetArmComponent();
+
+
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	float GetDefaultArmLength();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	float DefaultArmLength{ 50.0f };
 };
